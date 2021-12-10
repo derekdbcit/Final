35be25ad6c398ec8f99f9e440b50366da7bf05a8
derekdbcit <91754757+derekdbcit@users.noreply.github.com>
2021-12-10 13:17:29 -0800

    Dark Mode and Submit Message Attempt

---
+++ 1620_Final/JS/script.js
@@ -0,0 +1,21 @@
+var toggleTheme = document.getElementsByClassName("toggle");
+
+toggleTheme.onClick = function() {
+
+    if (document.getElementsByTagName("body").className == "light-theme") {
+        document.getElementsByTagName("body").className = "dark-theme";
+    } else {
+        document.getElementsByTagName("body").className = "light-theme";
+    }
+
+};
+
+var submitFakeEmail = document.getElementById("frm-btn");
+
+submitFakeEmail.onClick = function() {
+
+    console.log(document.getElementById("name").value);
+    console.log(document.getElementById("email").value);
+    console.log(document.getElementById("msg").value);
+
+}
